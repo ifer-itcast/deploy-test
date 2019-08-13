6f50472e62c399071ebdc0b206570bede6eb1564
ifer-itcast <yangkangkang@itcast.cn>
2019-08-13 22:41:26 +0800

    Updates

---
+++ precache-manifest.b4169071f0a612a02609a94a8f4c943b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f37c1fa126da2488efefb77665427e6",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "3f438b30267cf9946c6d",
+    "url": "/deploy-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5228b416e208ee4881d9",
+    "url": "/deploy-test/static/js/2.42f13203.chunk.js"
+  },
+  {
+    "revision": "3f438b30267cf9946c6d",
+    "url": "/deploy-test/static/js/main.c1721f6a.chunk.js"
+  },
+  {
+    "revision": "1271199fcbef7b2a66b7",
+    "url": "/deploy-test/static/js/runtime~main.5baf9d05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
